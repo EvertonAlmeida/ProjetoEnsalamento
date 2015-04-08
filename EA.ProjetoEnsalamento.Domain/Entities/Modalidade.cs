5c8dc769d38c40cc3185665729cc4b5724d7f722
Everton Almeida <everton.o.almeida@outlook.com>
2015-04-08 13:43:30 -0300

    add entidade e configurar conexao com BD atraves do CodeFirst

---
+++ EA.ProjetoEnsalamento.Domain/Entities/Modalidade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EA.ProjetoEnsalamento.Domain.Entities
+{
+    public class Modalidade
+    {
+        public Modalidade()
+        {
+            ModalidadeId = Guid.NewGuid();
+        }
+
+        public Guid ModalidadeId { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
